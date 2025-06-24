416c7359cb93457f5c22f8ce064952c5bc09e7d9
calebeynon <ceynon@crimson.ua.edu>
2025-06-24 16:52:13 -0400

    js based currently

--- livegame/marketgame2/__init__.py
+++ livegame/marketgame2/__init__.py
@@ -1,8 +1,6 @@
 from otree.api import *
 import random
-import matplotlib.pyplot as plt
-import os
-import uuid
+import json
 
 class C(BaseConstants):
     NAME_IN_URL = 'marketpage2'
@@ -22,10 +20,11 @@ class Subsession(BaseSubsession):
     pass
 
 class Group(BaseGroup):
-    current_price = models.FloatField(initial=C.INITIAL_PRICE)
+    current_price = models.FloatField(initial = C.INITIAL_PRICE)
     prior = models.FloatField(initial=C.PRIOR)
-    price_history = models.LongStringField(initial='[]')  # store as JSON string
-    belief_history = models.LongStringField(initial='[]')
+    poll_count = models.IntegerField(initial=0)
+    poll_players = models.LongStringField(initial='[]')
+    current_signal = models.IntegerField(initial=-1)
 
 class Player(BasePlayer):
     sold = models.BooleanField(initial=False)
@@ -51,93 +50,62 @@ class Market(Page):
         return C.TIMEOUT_SECONDS
     
     def vars_for_template(player):
-        group = player.group
-        import json
-        # Parse histories
-        price_hist = json.loads(group.price_history) if group.price_history else [C.INITIAL_PRICE]
-        belief_hist = json.loads(group.belief_history) if group.belief_history else [C.PRIOR*100]
-        # Generate and save graphs
-        static_dir = os.path.join('livegame', '_static', 'global')
-        os.makedirs(static_dir, exist_ok=True)
-        price_img = f'price_{uuid.uuid4().hex}.png'
-        belief_img = f'belief_{uuid.uuid4().hex}.png'
-        price_path = os.path.join(static_dir, price_img)
-        belief_path = os.path.join(static_dir, belief_img)
-        # Price graph
-        plt.figure()
-        plt.plot(price_hist, marker='o', color='red')
-        plt.title('Market Price Over Time')
-        plt.xlabel('Update Number')
-        plt.ylabel('Price')
-        plt.tight_layout()
-        plt.savefig(price_path)
-        plt.close()
-        # Belief graph
-        plt.figure()
-        plt.plot(belief_hist, marker='o', color='blue')
-        plt.title('Belief of Good State Over Time')
-        plt.xlabel('Update Number')
-        plt.ylabel('Belief (%)')
-        plt.tight_layout()
-        plt.savefig(belief_path)
-        plt.close()
-        # URLs for template
-        price_url = f'/static/global/{price_img}'
-        belief_url = f'/static/global/{belief_img}'
-        # Sold status
-        all_players = group.get_players()
-        sold_status = {p.id_in_group: p.sold for p in all_players}
-        return {
-            'initial_price': group.current_price,
-            'initial_prior': group.prior*100,
-            'price_img': price_url,
-            'belief_img': belief_url,
-            'sold_status': sold_status,
-            'self_sold': player.sold,
-        }
-
-    @staticmethod
+        return{'initial_price': C.INITIAL_PRICE,
+               'initial_prior': C.PRIOR*100}
+    
     def live_method(player, data):
-        import json
         group = player.group
-        # Load histories
-        price_hist = json.loads(group.price_history) if group.price_history else [C.INITIAL_PRICE]
-        belief_hist = json.loads(group.belief_history) if group.belief_history else [C.PRIOR*100]
+
+        price_history = json.loads(group.price_history)
+
         if data.get('action') == 'sell' and not player.sold:
             player.sold = True
             player.value = group.current_price
             group.current_price -= C.PRICE_DROP
+            price_history.append(group.current_price)
+
         if data.get('action') == 'poll':
-            # bayesian update
-            if C.STATE:
-                signal = 1 if random.random() < C.PCORRECT else 0
-                if signal:
-                    new_prior = (C.PCORRECT * group.prior) / ((C.PCORRECT * group.prior) + ((1 - C.PCORRECT) * (1 - group.prior)))
+            poll_players = json.loads(group.poll_players)
+            if player.id_in_group not in poll_players:
+                poll_players.append(player.id_in_group)
+                group.poll_count += 1
+                group.poll_players = json.dumps(poll_players)
+            if group.poll_count < C.PLAYERS_PER_GROUP:
+                return
+
+
+            ## bayesian update
+            # group prior is the belief that the asset is in good state
+            if group.current_signal == -1:
+                if C.STATE:
+                    signal = 1 if random.random() < C.PCORRECT else 0
+                    if signal:
+                        new_prior = (C.PCORRECT * group.prior) / ((C.PCORRECT * group.prior) + ((1 - C.PCORRECT) * (1 - group.prior)))
+                    else:
+                        new_prior = ((1 - C.PCORRECT) * group.prior) / (((1 - C.PCORRECT) * group.prior) + (C.PCORRECT * (1 - group.prior)))
                 else:
-                    new_prior = ((1 - C.PCORRECT) * group.prior) / (((1 - C.PCORRECT) * group.prior) + (C.PCORRECT * (1 - group.prior)))
-            else:
-                signal = 0 if random.random() < C.PCORRECT else 1
-                if signal:
-                    new_prior = (C.PCORRECT * group.prior) / ((C.PCORRECT * group.prior) + ((1 - C.PCORRECT) * (1 - group.prior)))
-                else:
-                    new_prior = ((1 - C.PCORRECT) * group.prior) / (((1 - C.PCORRECT) * group.prior) + (C.PCORRECT * (1 - group.prior)))
-            group.prior = new_prior
-        # Update histories
-        price_hist.append(group.current_price)
-        belief_hist.append(group.prior*100)
-        group.price_history = json.dumps(price_hist)
-        group.belief_history = json.dumps(belief_hist)
-        group.save()
+                    signal = 0 if random.random() < C.PCORRECT else 1
+                    if signal:
+                        new_prior = (C.PCORRECT * group.prior) / ((C.PCORRECT * group.prior) + ((1 - C.PCORRECT) * (1 - group.prior)))
+                    else:
+                        new_prior = ((1 - C.PCORRECT) * group.prior) / (((1 - C.PCORRECT) * group.prior) + (C.PCORRECT * (1 - group.prior)))
+                group.prior = new_prior
+
         all_players = group.get_players()
-        sold_status = {p.id_in_group: p.sold for p in all_players}
+        sold_status = {p.id_in_group: p.sold for p in all_players} 
+        group.poll_count = 0
+        group.poll_players = json.dumps([])
+        group.current_signal = -1
+        group.price_history = json.dumps(price_history)
         return {
             0: dict(
                 new_price=group.current_price,
                 old_price=group.current_price + C.PRICE_DROP,
                 sold_status=sold_status,
                 posterior=round(group.prior,2)*100,
-            )
+            )  
         }
+
 
     def before_next_page(player, timeout_happened):
         if C.STATE and not player.sold:
